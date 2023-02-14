2b654bc1bc4303b3409ca1b29bda6da81abd9303
Wassim <wassimchakib@hotmail.com>
2023-02-14 10:05:32 +0100

    Implement methods : can_be_archived? and move_to_archive

--- classes/item.rb
+++ classes/item.rb
@@ -1,25 +1,54 @@
+##
+# Item class takes 6 paramters
+# @genre is the categorie of item. ie: Music
+# @author is the owner of item. ie: book
+# @source
+# @label is the title of item. ie: album
+# @archived is the boolean value of item.
+# @publish_date is the published date of item.
+
 class Item
   attr_reader :genre, :author, :source, :label, :archived
 
-  def initialize(publish_date, archived)
+  def initialize(publish_date, archived: false)
     @id = Random.rand(1..10_000)
     @publish_date = publish_date
     @archived = archived
   end
 
+  # add_genre adds a genre to the item
+  # @param genre is the genre object
   def add_genre(genre)
     @genre = genre
   end
 
+  # add_source adds a source to the item
+  # @param source is the source object
   def add_source(source)
     @source = source
   end
 
+  # add_label adds a label to the item
+  # @param label is the label object
   def add_label(label)
     @label = label
   end
 
+  # add_author adds an author to the item
+  # @param author is the author object
   def add_author(author)
     @author = author
+  end
+
+  # move_to_archive change the value of archived if the item
+  # can be archived
+  def move_to_archive
+    @archived = true if can_be_archived
+  end
+
+  private
+
+  def can_be_archived?
+    publish_date > 10
   end
 end
