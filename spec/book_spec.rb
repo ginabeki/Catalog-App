21e8b00655ec0c76e3cc019e079ca4e2914e3863
ginabeki <ginabeki3@gmail.com>
2023-02-15 10:36:59 +0300

    Add unit tests for label and book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,15 +1,12 @@
 require './spec/spec_helper'
 describe Book do
-    before :all do
-      @book = Book.new('Literature', 'good', 12/10/2022)
+  before :all do
+    @book = Book.new('Literature', 'good', 12 / 10 / 2022)
+  end
+
+  describe '#new' do
+    it 'takes in three parameters and returns a book object' do
+      expect(@book).to be_an_instance_of Book
     end
-
-    describe '#new' do
-        it 'takes in three parameters and returns a book object' do
-          expect(@book).to be_an_instance_of Book
-        end
-    end
-
-    
-
+  end
 end
