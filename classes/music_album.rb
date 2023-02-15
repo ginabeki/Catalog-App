fd8e0bd5e89ccb57e4a0745d93379b738cd2fa00
Frehiywot Nega <fr.nega@gmail.com>
2023-02-15 12:18:05 +0300

    fix to lint error

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -4,7 +4,7 @@ class MusicAlbum < Item
   attr_accessor :on_spotify, :archived
   attr_reader :publish_date
 
-  def initialize(publish_date, on_spotify)
+  def initialize(publish_date, _on_spotify)
     super(publish_date)
     @on_spotify = on_spotifys
   end
