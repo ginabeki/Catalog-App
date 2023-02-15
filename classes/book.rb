76e23e2ff1ac3bf8710c5cf4ff772a4ebdc6b3d8
ginabeki <ginabeki3@gmail.com>
2023-02-15 09:37:22 +0300

    Include publish_date in initialize parameters and added equal operator to the can_be_archived method

--- classes/book.rb
+++ classes/book.rb
@@ -2,14 +2,15 @@ require_relative 'item'
 class Book < Item
   attr_reader :publisher, :cover_state
 
-  def initialize(publisher, cover_state)
-    super(publish_date, id)
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
 
   # @return boolean
+  private
   def can_be_archived
-    super || @cover_state = 'bad'
+    super || @cover_state == 'bad'
   end
 end
