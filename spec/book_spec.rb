ee04c60bedb5021f96582f7e3052c5dd3f26b7b4
ginabeki <ginabeki3@gmail.com>
2023-02-15 11:16:33 +0300

    Add test for book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -9,4 +9,16 @@ describe Book do
       expect(@book).to be_an_instance_of Book
     end
   end
+
+  it 'should return publisher' do
+    expect(@book.publisher).to eq 'Literature'
+  end
+
+  it 'should return cover_state' do
+    expect(@book.cover_state).to eq 'good'
+  end
+
+#   it 'should return publish_date' do
+#     expect(@book.publish_date).to eq 12 / 10 / 2022
+#   end
 end
