698c3fac1690369ff3e768d21598b17bff13c514
Wassim <wassimchakib@hotmail.com>
2023-02-16 14:35:28 +0100

    Fix schema typo + add Item table

--- schema.sql
+++ schema.sql
@@ -1,13 +1,3 @@
--- Create Game table 
-CREATE TABLE Game {
-  id INTEGER GENERATED ALWAYS AS IDENTITY,
-  multiplayer BOOLEAN NOT NULL,
-  last_played_at DATE NOT NULL
-  item_id INTEGER,
-  PRIMARY KEY(id),
-  CONSTRAINT fk_item FOREIGN KEY(item_id) references Item(id) ON DELETE CASCADE
-};
-
 -- Create Author table 
 CREATE TABLE Author {
   id INTEGER GENERATED ALWAYS AS IDENTITY,
@@ -15,3 +5,28 @@ CREATE TABLE Author {
   last_name VARCHAR(100),
   PRIMARY KEY(id)
 };
+
+-- Create Item table
+CREATE TABLE item (
+	id  INT GENERATED ALWAYS AS IDENTITY,
+	genre_id INT,
+	author_id INT,
+	label_id INT,
+	publish_date DATE,
+	archived BOOLEAN,
+	PRIMARY KEY(id),
+	FOREIGN KEY (genre_id) REFERENCES genres (id),
+	FOREIGN KEY (author_id) REFERENCES author (id),
+	FOREIGN KEY (label_id) REFERENCES label (id)
+);
+
+-- Create Game table 
+CREATE TABLE Game {
+  id INTEGER GENERATED ALWAYS AS IDENTITY,
+  multiplayer BOOLEAN NOT NULL,
+  last_played_at DATE NOT NULL,
+  item_id INTEGER,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_item FOREIGN KEY(item_id) references item(id) ON DELETE CASCADE
+};
+
